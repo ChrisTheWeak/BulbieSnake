6535dc86da56753cd7d3ae3f98ec09e97e439514
Atreyu Schilling <aschilling@rtautomation.com>
2026-06-15 02:38:41 -0500

    Dist subtree

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  publicDir: process.env.NODE_ENV === 'production' ? '/BulbieSnake/' : '/'
+  base: process.env.NODE_ENV === "production" ? "/BulbieSnake/" : "/",
   plugins: [
     vue(),
     vueDevTools(),
